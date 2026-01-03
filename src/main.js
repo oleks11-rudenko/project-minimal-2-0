dbba8771af021ceeb191b95b19519e0dae2574c6
oleksiirudenko <or190802@gmail.com>
2026-01-03 14:02:29 +0200

    Add mobile menu, change some css, add terms and privacy pages

--- src/main.js
+++ src/main.js
@@ -1,6 +1,31 @@
+const menuBtnOpen = document.querySelector('.js-open-menu');
+const menuBtnClose = document.querySelector('.js-close-menu');
+const modal = document.querySelector('.js-modal');
+const modalLinks = document.querySelectorAll('.modal-link');
+
 const form = document.querySelector('.newsletter-form');
 const email = document.querySelector('.emailInput');
 const emailPattern = /^[a-zA-Z0-9._-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,6}$/;
+
+const openModal = () => {
+  menuBtnOpen.classList.add('hide');
+  menuBtnClose.classList.remove('hide');
+  modal.classList.add('is-open');
+  document.body.style.overflow = 'hidden';
+};
+
+const closeModal = () => {
+  menuBtnOpen.classList.remove('hide');
+  menuBtnClose.classList.add('hide');
+  modal.classList.remove('is-open');
+  document.body.style.overflow = '';
+};
+
+modalLinks.forEach((link) => {
+  link.addEventListener('click', function () {
+    closeModal();
+  });
+});
 
 const handleSubmit = (event) => {
   event.preventDefault();
@@ -19,6 +44,8 @@ const handleInput = () => {
   }
 };
 
-form.addEventListener('submit', handleSubmit);
+menuBtnOpen.addEventListener('click', openModal);
+menuBtnClose.addEventListener('click', closeModal);
 
+form.addEventListener('submit', handleSubmit);
 email.addEventListener('input', handleInput);
